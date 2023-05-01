09ab843733f751567199c24bfb618c1ab0f9fb2d
panlin520 <40063365+panlin520@users.noreply.github.com>
2023-05-02 04:18:28 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "ChatGPT Next 1111",
+  title: "ChatGPT Next Web",
   description: "Your personal ChatGPT Chat Bot.",
   appleWebApp: {
     title: "ChatGPT Next Web",
